b17efd7003ef6f246aaf46f11f7b5d088655f934
hendisantika <hendisantika@yahoo.co.id>
2024-12-07 06:27:19 +0700

    EnableScheduling🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/distributedscheduledlocksredis/SpringBootDistributedScheduledLocksRedisApplication.java
+++ src/main/java/id/my/hendisantika/distributedscheduledlocksredis/SpringBootDistributedScheduledLocksRedisApplication.java
@@ -2,8 +2,10 @@ package id.my.hendisantika.distributedscheduledlocksredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootDistributedScheduledLocksRedisApplication {
 
     public static void main(String[] args) {
